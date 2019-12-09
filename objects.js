677f4526cdee771f94ec11df5091d5deeb8cdead
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-09 23:59:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
-var playlist = {artist : "song"};
- playlist
+var playlist = {
+  Phil Ochs : "Here's To The State Of Mississippi"
+}
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist['Slowdive'] = 'Alison'
