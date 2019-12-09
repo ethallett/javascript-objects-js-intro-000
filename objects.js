08bad9926cec9aa5831050253c13171297ee30d9
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-09 23:47:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({artistName: songTitle})
+var playlist = new Object (Hayes: "Beaumont"})
 
 function updatePlaylist(playlist, artistName, songTitle){
   Object['artistName'] = 'songtitle'
